1dd44b480a7e01433999b80c0b04c437f288228a
danube88 <36491066+danube88@users.noreply.github.com>
2020-07-02 01:26:36 +0300

    create package "mcamara/laravel-localization"

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group([
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
+    ],
+    function ()
+    {
+        Route::get('/', function () {
+            return view('welcome');
+        });
 
-Auth::routes();
+        Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
