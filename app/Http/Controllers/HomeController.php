7ccd11e7049ce69cfb5c4804fe2ba9857a76dbf3
danube88 <36491066+danube88@users.noreply.github.com>
2020-07-13 01:00:58 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,28 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $user = User::find(Auth::id());
+
+        if ($user->hasRole('admin'))
+        { // you can pass an id or slug
+            if(view()->exists('admin.index'))
+            {
+                return view('admin.index');
+            }
+
+            abort(404);
+        }
+
+        if ($user->hasRole('user'))
+        { // you can pass an id or slug
+            if(view()->exists('user.index'))
+            {
+                return view('user.index');
+            }
+
+            abort(404);
+        }
+
         return view('home');
     }
 }
