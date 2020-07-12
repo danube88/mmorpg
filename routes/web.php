45369912a20873dfeb742e57440231f2cb63e99c
danube88 <36491066+danube88@users.noreply.github.com>
2020-07-13 01:00:03 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-        'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
-    ],
-    function ()
-    {
-        Route::get('/', function () {
-            return view('welcome');
-        });
-
-        Auth::routes();
-
-        Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],], function () {
+    Route::get('/', function () {
+        return view('welcome');
     });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::group(['prefix' => 'admin','middleware' => ['role:admin']], function () {
+        //Route::get('/home', 'HomeController@index');
+    });
+
+    Route::group(['prefix' => 'user', 'middleware' => ['role:user', 'role:admin']], function () {
+        //Route::get('/home', 'HomeController@index');
+    });
+});
